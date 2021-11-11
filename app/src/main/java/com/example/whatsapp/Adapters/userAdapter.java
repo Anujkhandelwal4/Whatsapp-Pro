984ab41728e6e6e1b22f5e409e579015161401aa
admin <anujkhandelwal.2cse22@jecrc.ac.in>
2021-11-11 21:46:18 +0530

    Whatsapp pro

---
+++ app/src/main/java/com/example/whatsapp/Adapters/userAdapter.java
@@ -0,0 +1,29 @@
+package com.example.whatsapp.Adapters;
+
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.whatsapp.R;
+
+public class userAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView userName , lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.profile_image);
+            userName = itemView.findViewById(R.id.userNameList);
+            lastMessage = itemView.findViewById(R.id.lastMessage);
+
+        }
+    }
+}
+
